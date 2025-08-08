d4789000433334807b53c810962b02ec6290b20e
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-08 15:30:23 +0700

    feat: add icon components

---
+++ client/src/components/common/icons/HomeIcon.tsx
@@ -0,0 +1,25 @@
+const HomeIcon = () => {
+  return (
+    <svg
+      className="w-5 h-5"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2H5a2 2 0 00-2-2z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M8 5a2 2 0 012-2h4a2 2 0 012 2v0a2 2 0 01-2 2H10a2 2 0 01-2-2v0z"
+      />
+    </svg>
+  );
+};
+
+export default HomeIcon;
