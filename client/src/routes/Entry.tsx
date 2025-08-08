dd698b5c1feabbdfe2ad9919d9944dca100c49e2
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-08 13:44:52 +0700

    feat(entry): add Spinner loading and improve auth handling

--- client/src/routes/Entry.tsx
+++ client/src/routes/Entry.tsx
@@ -7,11 +7,12 @@ import useCurrentUser from "../hooks/useCurrentUser";
 import LoginView from "../pages/Auth/LoginView";
 import { useEffect } from "react";
 import { checkAuthStatus } from "../redux/slices/AuthSlices";
+import Spinner from "../components/common/loading/Spinner";
 
 export const Entry = () => {
   const dispatch = useAppDispatch();
   const { isLoggedin, isInitializing } = useAppSelector((state) => state.auth);
-  const { user } = useCurrentUser();
+  const { user, loading: userLoading } = useCurrentUser();
 
   useEffect(() => {
     if (isInitializing) {
@@ -23,16 +24,12 @@ export const Entry = () => {
     return <LoginView />;
   }
 
+  if (isInitializing || (isLoggedin && userLoading)) {
+    return <Spinner />;
+  }
   if (isLoggedin) {
     if (!user) {
-      return (
-        <div className="min-h-screen flex items-center justify-center">
-          <div className="text-center">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 mx-auto mb-4"></div>
-            <p className="text-gray-600">Loading user info...</p>
-          </div>
-        </div>
-      );
+      return <Spinner />;
     }
     switch (user.role) {
       case "ROLE_ADMIN":
