57f9c3c46bd93ce6ba42b0216cda6341a7d0ae04
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-10 17:08:33 +0700

    feat(router): add RouteRender to customers and upload routes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,8 +1,17 @@
 import { Entry } from "./Entry";
+import RouteRender from "./RouteRender";
 
 export const routes = [
   {
     path: "/",
     element: <Entry />,
+  },
+  {
+    path: "customers",
+    element: <RouteRender />,
+  },
+  {
+    path: "upload",
+    element: <RouteRender />,
   },
 ];
