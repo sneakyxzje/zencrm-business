936b878e6b0e71349c3555a1d5ea853c65e7695a
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-10 17:06:43 +0700

    feat(router): add RouterRender for auth and role-based routing

---
+++ client/src/routes/RouteRender.tsx
@@ -0,0 +1,32 @@
+import { useLocation } from "react-router-dom";
+import Spinner from "../components/common/loading/Spinner";
+import { RouteConfig } from "./RouteConfig";
+import { ErrorPage } from "../pages/Error";
+import { useEffect } from "react";
+import { useAppDispatch } from "../redux/Hooks";
+import { checkAuthStatus } from "../redux/slices/AuthSlices";
+import { useAuth } from "../hooks/useAuth";
+
+const RouteRender = () => {
+  const dispatch = useAppDispatch();
+  const { pathname } = useLocation();
+  const { user, isInitializing, isLoggingin } = useAuth();
+
+  useEffect(() => {
+    if (isInitializing) {
+      dispatch(checkAuthStatus());
+    }
+  }, [dispatch, isInitializing]);
+
+  if (isLoggingin || isInitializing) return <Spinner />;
+  if (!user) {
+    return <h1>You are not signin</h1>;
+  }
+
+  const componentMap = RouteConfig[pathname];
+  if (!componentMap) return <ErrorPage />;
+
+  return componentMap[user.role] ?? <ErrorPage />;
+};
+
+export default RouteRender;
