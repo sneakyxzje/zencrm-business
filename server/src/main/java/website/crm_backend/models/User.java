e97eab8bc3b6c9db9271a6e72b687aa9c4a3e3bd
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-10 17:17:17 +0700

    feat(user): use enum for role, add unique email and indexes

--- server/src/main/java/website/crm_backend/models/User.java
+++ server/src/main/java/website/crm_backend/models/User.java
@@ -2,29 +2,43 @@ package website.crm_backend.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import website.crm_backend.models.enums.*;
 
 @Entity
-@Table(name = "users")
+@Table(
+    name = "users",
+    uniqueConstraints = @UniqueConstraint(
+        name = "uk_users_email",
+        columnNames = "email"
+    ),
+    indexes = @Index(name = "idx_users_role", columnList = "role")
+)
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "fullname")
+    @Column(name = "fullname", nullable = false, length=50)
     private String fullname;    
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, length=100)
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false, length=255)
     private String password;
     
-    @Column(name = "role")
-    private String role;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role = Role.ROLE_MARKETING;
 
     public User() {}
 
@@ -32,7 +46,7 @@ public class User {
         return id;
     }
 
-    public String getFullName() {
+    public String getFullname() {
         return fullname;
     }
 
@@ -44,9 +58,10 @@ public class User {
         return password;
     }
 
-    public String getRole() {
+    public Role getRole() {
         return role;
     }
+
 
     public void setFullname(String fullname) {
         this.fullname = fullname;
@@ -60,7 +75,7 @@ public class User {
         this.password = password;
     }
 
-    public void setRole(String role) {
+    public void setRole(Role role) {
         this.role = role;
     }
 }
