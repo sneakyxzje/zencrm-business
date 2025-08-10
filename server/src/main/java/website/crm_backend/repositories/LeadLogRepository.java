ddccf6901289908593c91a537fa1e257da6ae541
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-10 17:29:59 +0700

    feat(repo): add LeadLogRepository

---
+++ server/src/main/java/website/crm_backend/repositories/LeadLogRepository.java
@@ -0,0 +1,16 @@
+package website.crm_backend.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import website.crm_backend.models.LeadLog;
+import website.crm_backend.models.enums.LogAction;
+
+public interface LeadLogRepository extends JpaRepository<LeadLog, Integer> {
+    Page<LeadLog> findByLead_IdOrderByCreatedAtDesc(int leadId, Pageable pageable);
+    Page<LeadLog> findByActor_IdOrderByCreatedAtDesc(int actorUserId, Pageable pageable);
+    List<LeadLog> findTop50ByActionOrderByCreatedAtDesc(LogAction action);
+}
