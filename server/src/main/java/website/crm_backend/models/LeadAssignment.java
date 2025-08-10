3eb06fd7944202f708ec50287703822e08dc559f
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-10 17:26:44 +0700

    feat(models): add LeadAssignment entity

---
+++ server/src/main/java/website/crm_backend/models/LeadAssignment.java
@@ -0,0 +1,42 @@
+package website.crm_backend.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="lead_assignment")
+public class LeadAssignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="lead_id", foreignKey = @ForeignKey(name="fk_assignment_lead"))
+    private Lead lead;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="assignee_user_id", foreignKey = @ForeignKey(name="fk_assignee_user_id"))
+    private User assignee;
+
+    private LocalDateTime assignedAt;
+    private LocalDateTime unAssignedAt;
+
+    @PrePersist
+    protected void onAssign() {
+        this.assignedAt = LocalDateTime.now();
+    }
+}
