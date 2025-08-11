1005fa9f3ca8c75b540d0c38425cb10b13ba9434
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-11 23:52:16 +0700

    refactor: use request/response DTOs

--- server/src/main/java/website/crm_backend/controllers/LeadController.java
+++ server/src/main/java/website/crm_backend/controllers/LeadController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import website.crm_backend.DTOS.LeadListDTO;
-import website.crm_backend.DTOS.LeadResponseDTO;
-import website.crm_backend.DTOS.UploadLeadDTO;
+import website.crm_backend.DTOS.LeadDTO.LeadListDTO;
+import website.crm_backend.DTOS.request.UploadLeadRequest;
+import website.crm_backend.DTOS.response.UploadLeadResponse;
 import website.crm_backend.models.Lead;
 import website.crm_backend.services.LeadService;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,12 +30,12 @@ public class LeadController {
 
 
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadLead(@RequestBody UploadLeadDTO request) {
+    public ResponseEntity<?> uploadLead(@RequestBody UploadLeadRequest request) {
         if(request.getPhoneNumber() == null || request.getPhoneNumber().isBlank() ) {
             return ResponseEntity.badRequest().body("Phone number is required");
         }
         Lead lead = leadService.uploadNumber(request.getPhoneNumber());
-        LeadResponseDTO dto = new LeadResponseDTO(
+        UploadLeadResponse dto = new UploadLeadResponse(
             lead.getId(),
             lead.getPhone().getNumber(),
             lead.getCreatedBy().getId(),
