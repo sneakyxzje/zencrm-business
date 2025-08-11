41b754be492c6492d8fe4dbc611672f5e5fc156b
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-12 00:02:55 +0700

    feat(models): add Team entity

---
+++ server/src/main/java/website/crm_backend/models/Team.java
@@ -0,0 +1,36 @@
+package website.crm_backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import website.crm_backend.models.enums.TeamType;
+
+@Entity
+@Data
+@Table(name="teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String teamName;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="fk_manager_id", foreignKey = @ForeignKey(name = "fk_manager_team_id"))
+    private User manager;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TeamType teamType;
+}
