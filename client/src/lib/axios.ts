ba6bb33d645cc02170237133f3b9fa61a524562e
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-15 16:24:56 +0700

    chore(api): add custom query params serializer using qs

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-
+import * as qs from "qs";
 export const api = axios.create({
   baseURL: "http://localhost:8080",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
 });
