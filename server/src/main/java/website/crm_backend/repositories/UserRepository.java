83c9fb6f2f129797e61329b3cccc320b7ff2ecc5
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-19 12:20:46 +0700

    feat(repository): add JpaSpecificationExecutor and override findAll with entity graph

--- server/src/main/java/website/crm_backend/repositories/UserRepository.java
+++ server/src/main/java/website/crm_backend/repositories/UserRepository.java
@@ -1,14 +1,19 @@
 package website.crm_backend.repositories;
-
-
-
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import website.crm_backend.models.User;
 
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
     boolean existsByEmail(String email);
     User findByEmail(String email);
+
+    @Override
+    @EntityGraph(attributePaths = {"team","team.manager"})
+    Page<User> findAll(Specification<User> spec, Pageable pageable);
 }
