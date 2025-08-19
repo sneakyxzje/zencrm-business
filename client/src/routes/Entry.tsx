a067088cf0baf6dbda0c78acd25902afb7e97ad3
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-19 20:40:36 +0700

    feat(entry): add route for Sale Manager role
    - import SaleManager view
    - handle ROLE_SALE_MANAGER in Entry component switch case

--- client/src/routes/Entry.tsx
+++ client/src/routes/Entry.tsx
@@ -8,6 +8,7 @@ import { checkAuthStatus } from "../redux/slices/AuthSlices";
 import Spinner from "../components/common/loading/Spinner";
 import { useAuth } from "../hooks/useAuth";
 import { ErrorPage } from "../pages/Error";
+import SaleManager from "../pages/SaleManager/SaleManager";
 
 export const Entry = () => {
   const dispatch = useAppDispatch();
@@ -40,6 +41,8 @@ export const Entry = () => {
       return <SaleView />;
     case "ROLE_MARKETING_MANAGER":
       return <MarketingView />;
+    case "ROLE_SALE_MANAGER":
+      return <SaleManager />;
     default:
       return <ErrorPage />;
   }
