9b219e27a73932c1a0063c6c3441020459acbf88
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-19 11:58:50 +0700

    refactor(lead): restructure upload logic and update authorization roles

--- server/src/main/java/website/crm_backend/controllers/LeadController.java
+++ server/src/main/java/website/crm_backend/controllers/LeadController.java
@@ -1,18 +1,23 @@
 package website.crm_backend.controllers;
 
 
+import java.util.Set;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import website.crm_backend.DTOS.LeadDTO.LeadListDTO;
+import website.crm_backend.DTOS.request.AssignLeadRequest;
 import website.crm_backend.DTOS.request.UploadLeadRequest;
+import website.crm_backend.DTOS.response.AssignLeadResponse;
 import website.crm_backend.DTOS.response.UploadLeadResponse;
-import website.crm_backend.models.Lead;
+import website.crm_backend.models.enums.LeadStatus;
 import website.crm_backend.services.LeadService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,31 +25,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 
+
 @RestController
 @RequestMapping("/api/leads")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('ROLE_MARKETING')")
 public class LeadController {
     
     private final LeadService leadService;
 
-
+    @PreAuthorize("hasRole('MARKETING', 'MARKETING_MANAGER')")
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadLead(@RequestBody UploadLeadRequest request) {
-        if(request.getPhoneNumber() == null || request.getPhoneNumber().isBlank() ) {
-            return ResponseEntity.badRequest().body("Phone number is required");
-        }
-        Lead lead = leadService.uploadNumber(request.getPhoneNumber());
-        UploadLeadResponse dto = new UploadLeadResponse(
-            lead.getId(),
-            lead.getPhone().getNumber(),
-            lead.getCreatedBy().getId(),
-            lead.getCreatedBy().getFullname(),
-            lead.getStatus().name(),
-            lead.getCreatedAt()
-        );
-        return ResponseEntity.ok(dto);
+    public ResponseEntity<UploadLeadResponse> uploadLead(@RequestBody UploadLeadRequest request) {
+        return ResponseEntity.ok(leadService.uploadLead(request));        
     } 
+
+    // For marketing
+    @PreAuthorize("hasRole('MARKETING', 'MARKETING_MANAGER')")
     @GetMapping("/list")    
     public ResponseEntity<Page<LeadListDTO>> getAllLeads(Pageable pageable) {
         return ResponseEntity.ok(leadService.getAllLeads(pageable));
