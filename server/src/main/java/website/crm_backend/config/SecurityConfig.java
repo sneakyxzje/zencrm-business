ed4e2448c2ad982f55ae0576455f2170d66f0d50
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-20 16:47:54 +0700

    chore(security): enable method-level security with @EnableMethodSecurity

--- server/src/main/java/website/crm_backend/config/SecurityConfig.java
+++ server/src/main/java/website/crm_backend/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,7 @@ import website.crm_backend.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     
     @Autowired
