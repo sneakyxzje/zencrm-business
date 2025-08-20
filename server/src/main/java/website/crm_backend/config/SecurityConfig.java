42d7feb3024f9f1eff779e8c029e64801b61707e
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-20 16:49:55 +0700

    chore(security): enable method-level security

--- server/src/main/java/website/crm_backend/config/SecurityConfig.java
+++ server/src/main/java/website/crm_backend/config/SecurityConfig.java
@@ -24,7 +24,7 @@ import website.crm_backend.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity 
 public class SecurityConfig {
     
     @Autowired
@@ -67,7 +67,6 @@ public class SecurityConfig {
         )
         .authenticationProvider(authenticationProvider())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        
         return http.build();
     }
 
