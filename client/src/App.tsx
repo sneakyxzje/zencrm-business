11d007f790c4f88bde40f07b89e6b72b40b0e070
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-22 22:18:27 +0700

    refactor(app): wrap routes with ToastProvider & update page imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,73 +2,76 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthBootstrap, GuestRoute } from "./routes/guards";
 import RoleRoute from "./routes/RoleRoute";
 import LoginView from "@pages/auth/login";
-import MarketingView from "@pages/marketing";
-import SaleView from "@pages/sale";
-import SaleManager from "@pages/saleManager";
-import MarketingCustomer from "@pages/marketing/customers";
 import { ErrorPage } from "@pages/error";
-import MarketingUploadPage from "@pages/marketing/upload/MarketingUpload";
+import MarketingView from "@pages/Marketing";
+import SalePage from "@pages/Sale";
+import SaleManager from "@pages/SaleManager";
+import MarketingCustomersPage from "@pages/Marketing/customers";
+import MarketingUploadPage from "@pages/Marketing/upload/MarketingUpload";
+import { ToastProvider } from "@app/provider/ToastProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AuthBootstrap />}>
-          <Route
-            path="/login"
-            element={
-              <GuestRoute>
-                <LoginView />
-              </GuestRoute>
-            }
-          />
+    <ToastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AuthBootstrap />}>
+            <Route
+              path="/login"
+              element={
+                <GuestRoute>
+                  <LoginView />
+                </GuestRoute>
+              }
+            />
 
-          <Route
-            path="/"
-            element={
-              <RoleRoute
-                map={{
-                  ROLE_MARKETING: <MarketingView />,
-                  ROLE_MARKETING_MANAGER: <MarketingView />,
-                  ROLE_SALE: <SaleView />,
-                  ROLE_SALE_MANAGER: <SaleManager />,
-                }}
-              />
-            }
-          />
+            <Route
+              path="/"
+              element={
+                <RoleRoute
+                  map={{
+                    ROLE_MARKETING: <MarketingView />,
+                    ROLE_MARKETING_MANAGER: <MarketingView />,
+                    ROLE_SALE: <SalePage />,
+                    ROLE_SALE_MANAGER: <SaleManager />,
+                  }}
+                />
+              }
+            />
 
-          {/* /customers */}
-          <Route
-            path="/customers"
-            element={
-              <RoleRoute
-                map={{
-                  ROLE_MARKETING: <MarketingCustomer />,
-                  ROLE_MARKETING_MANAGER: <MarketingCustomer />,
-                  ROLE_SALE: <SaleView />,
-                }}
-              />
-            }
-          />
+            {/* /customers */}
+            <Route
+              path="/customers"
+              element={
+                <RoleRoute
+                  map={{
+                    ROLE_MARKETING: <MarketingCustomersPage />,
+                    ROLE_MARKETING_MANAGER: <MarketingCustomersPage />,
+                    ROLE_SALE: <SalePage />,
+                  }}
+                />
+              }
+            />
 
-          <Route
-            path="/upload"
-            element={
-              <RoleRoute
-                map={{
-                  ROLE_MARKETING: <MarketingUploadPage />,
-                  ROLE_MARKETING_MANAGER: <MarketingUploadPage />,
-                }}
-              />
-            }
-          />
+            <Route
+              path="/upload"
+              element={
+                <RoleRoute
+                  map={{
+                    ROLE_MARKETING: <MarketingUploadPage />,
+                    ROLE_MARKETING_MANAGER: <MarketingUploadPage />,
+                  }}
+                />
+              }
+            />
 
-          {/* 403 & 404 */}
-          <Route path="/403" element={<ErrorPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            {/* 403 & 404 */}
+            <Route path="/403" element={<ErrorPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ToastProvider>
   );
 }
 
