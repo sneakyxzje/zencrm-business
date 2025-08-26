35849cb1387e0e65db79d33e7128d61861c83d29
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-26 16:23:44 +0700

    feat(logs): add IN_PROGRESS log action type

--- server/src/main/java/website/crm_backend/models/enums/LogAction.java
+++ server/src/main/java/website/crm_backend/models/enums/LogAction.java
@@ -1,5 +1,5 @@
 package website.crm_backend.models.enums;
 
 public enum LogAction {
-    UPLOAD_NEW, UPLOAD_DUPLICATE, ASSIGN, STATUS_CHANGE, NOTE_ADDED
+    UPLOAD_NEW, UPLOAD_DUPLICATE, ASSIGN, STATUS_CHANGE, NOTE_ADDED, IN_PROGRESS
 }
