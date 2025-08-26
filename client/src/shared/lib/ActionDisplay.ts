5cd1f4f4dfab4789263b79d5da04dfd5e403b16c
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-26 16:19:07 +0700

    feat(logs): handle new log action types

--- client/src/shared/lib/ActionDisplay.ts
+++ client/src/shared/lib/ActionDisplay.ts
@@ -2,7 +2,17 @@ export const ActionDisplay = (act: string) => {
   switch (act) {
     case "UPLOAD_NEW":
       return "created";
+    case "NEW":
+      return "new lead";
+    case "ASSIGNED":
+      return "assigned";
     case "ASSIGN":
       return "assigned";
+    case "STATUS_CHANGE":
+      return "change status";
+    case "IN_PROGRESS":
+      return "in progress";
+    case "CALLED":
+      return "called";
   }
 };
