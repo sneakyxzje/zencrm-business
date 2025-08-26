4ee043131ad601e2529a0399794a34cffaf2ce39
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-26 16:10:54 +0700

    feat(leads): include status in update lead payload

--- client/src/features/update-lead/model/types.ts
+++ client/src/features/update-lead/model/types.ts
@@ -1,6 +1,10 @@
 import type { LeadStatus } from "@entities/lead/model/types";
 
-export type UpdateLeadPayLoad = { leadId: number; note: string };
+export type UpdateLeadPayLoad = {
+  leadId: number;
+  note: string;
+  status: string;
+};
 
 export type UpdateLeadResponse = {
   note: string;
