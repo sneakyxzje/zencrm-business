915940c18a1dcc8d5a583db79f2f9ce94cfffe16
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-26 16:21:48 +0700

    feat(leads): include status in update lead payload

--- server/src/main/java/website/crm_backend/DTOS/request/UpdateLeadRequest.java
+++ server/src/main/java/website/crm_backend/DTOS/request/UpdateLeadRequest.java
@@ -1,9 +1,11 @@
 package website.crm_backend.DTOS.request;
 
+import website.crm_backend.models.enums.LeadStatus;
 
 public record UpdateLeadRequest(
     Integer leadId,
-    String note
+    String note,
+    LeadStatus status
 ) {
     
 }
