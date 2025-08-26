5066d62416c28069808112853b82663a2c392ff4
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-26 16:09:52 +0700

    refactor(leads): align StatusBadge with new status model

--- client/src/entities/lead/ui/StatusBadge.tsx
+++ client/src/entities/lead/ui/StatusBadge.tsx
@@ -6,12 +6,9 @@ function getStatusText(status: LeadStatus) {
       return "Mới";
     case "CALLED":
       return "Đã gọi";
-    case "interested":
-      return "Quan tâm";
-    case "not_interested":
-      return "Không quan tâm";
-    case "converted":
-      return "Đã chuyển đổi";
+    case "ASSIGNED":
+      return "ASSIGNED";
+
     default:
       return "Khác";
   }
@@ -22,12 +19,8 @@ function badgeClass(s: LeadStatus) {
       return "bg-[#27292b] text-[#e6e6e6] border border-[#3f4245]";
     case "CALLED":
       return "bg-[#1f2a2f]/60 text-[#9fd6ff] border border-[#28414a]";
-    case "interested":
-      return "bg-[#1f2a22]/60 text-[#a3e1b7] border border-[#274133]";
-    case "converted":
+    case "ASSIGNED":
       return "bg-[#2a2520]/60 text-[#ffd5b3] border border-[#4a3b2f]";
-    case "not_interested":
-      return "bg-[#2b2224]/60 text-[#f1a9a9] border border-[#4a2b30]";
     default:
       return "bg-[#27292b] text-[#cfd4d5] border border-[#3f4245]";
   }
