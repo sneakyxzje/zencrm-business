1bbb69ce386af1284cda7d03b58860115d0bab18
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 21:18:05 +0700

    refactor(auth): delegate auth and cookie logic to AuthService

--- server/src/main/java/website/crm_backend/features/auths/controllers/AuthController.java
+++ server/src/main/java/website/crm_backend/features/auths/controllers/AuthController.java
@@ -2,10 +2,9 @@ package website.crm_backend.features.auths.controllers;
 
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,18 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import website.crm_backend.features.auths.dtos.request.AuthLoginRequest;
 import website.crm_backend.features.auths.dtos.request.AuthRegisterRequest;
 import website.crm_backend.features.auths.dtos.response.AuthRegisterResponse;
 import website.crm_backend.features.auths.services.AuthService;
-import website.crm_backend.shared.security.JwtTokenProvider;
-import website.crm_backend.shared.security.UserDetailsImpl;
 import website.crm_backend.shared.utils.AuthUtils;
 
 
@@ -35,11 +28,6 @@ import website.crm_backend.shared.utils.AuthUtils;
 public class AuthController {
     private final AuthService authService;
 
-    private final AuthenticationManager authenticationManager;
-
-    private final JwtTokenProvider jwtTokenProvider;
-
-
     @PostMapping("/register")
     public ResponseEntity<AuthRegisterResponse> register(@Validated @RequestBody AuthRegisterRequest request) {
         AuthRegisterResponse response = authService.registerUser(request);
@@ -47,47 +35,22 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthLoginRequest request, HttpServletResponse response) {
-
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-            request.email(),
-            request.password()
-            )
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String role = authentication.getAuthorities().iterator().next().getAuthority();
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        int userId = userDetails.getId();
-        String token = jwtTokenProvider.generateToken(userId, role);
-
-        ResponseCookie jwtCookie = ResponseCookie.from("jwt", token)
-        .httpOnly(true)
-        .secure(false) 
-        .path("/")
-        .maxAge(24 * 60 * 60)
-        .sameSite("Strict")
-        .build();
-
-        response.addHeader("Set-Cookie", jwtCookie.toString());
-        return ResponseEntity.ok("Login success");
+    public ResponseEntity<?> login(@RequestBody AuthLoginRequest request) {
+        ResponseCookie jwtCookie = authService.login(request);
+        return ResponseEntity.ok()
+        .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+        .body("Login success");
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletResponse response) {
-        ResponseCookie jwt = ResponseCookie.from("jwt", "")
-        .httpOnly(true)
-        .secure(false)
-        .path("/")
-        .maxAge(0)
-        .sameSite("Strict")
+    public ResponseEntity<Void> logout() {
+        ResponseCookie jwtCookie = authService.logout();
+        return ResponseEntity.noContent()
+        .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
         .build();
-
-        response.addHeader(("Set-Cookie"), jwt.toString());
-
-        return ResponseEntity.noContent().build(); // 204
     }
+
+    // Refactor later
     @GetMapping("/info")
     public ResponseEntity<?> getInfo(HttpServletRequest request) {
         var res = Map.of(
