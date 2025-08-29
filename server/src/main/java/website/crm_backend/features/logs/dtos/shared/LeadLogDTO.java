bffe257b066f612b2b7e2448e7fc5468201e7721
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 16:15:22 +0700

    chore(structure): moves file

---
+++ server/src/main/java/website/crm_backend/features/logs/dtos/shared/LeadLogDTO.java
@@ -0,0 +1,18 @@
+package website.crm_backend.features.logs.dtos.shared;
+
+import java.time.LocalDateTime;
+
+import website.crm_backend.models.enums.LeadStatus;
+import website.crm_backend.models.enums.LogAction;
+
+public record LeadLogDTO(
+    Integer id,
+    LogAction action,
+    String actorName,
+    LocalDateTime createdAt,
+    String targetUser,
+    LeadStatus fromStatus,
+    LeadStatus toStatus
+) {
+    
+}
---
+++ server/src/main/java/website/crm_backend/features/logs/services/LogService.java
@@ -0,0 +1,24 @@
+package website.crm_backend.features.logs.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import website.crm_backend.features.logs.dtos.shared.LeadLogDTO;
+import website.crm_backend.models.LeadLog;
+import website.crm_backend.repositories.LeadLogRepository;
+import website.crm_backend.shared.mapper.LogMapper;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+    
+    private final LogMapper logMapper;
+    private final LeadLogRepository leadLogRepo;
+
+    public Page<LeadLogDTO> getLogByLeadId(Integer leadId, Pageable pageable) {
+        Page<LeadLog> log = leadLogRepo.findByLead_Id(leadId, pageable);
+        return log.map(logMapper::toLeadLogDTO);
+    }
+}
