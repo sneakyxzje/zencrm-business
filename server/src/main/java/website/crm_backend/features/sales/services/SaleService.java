66e4cf59b41c30fc32a41852dc2421bcfff6c466
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 16:25:18 +0700

    refactor(services): extract services for sale from leadService

---
+++ server/src/main/java/website/crm_backend/features/sales/services/SaleService.java
@@ -0,0 +1,73 @@
+package website.crm_backend.features.sales.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import website.crm_backend.features.leads.dtos.shared.LeadListDTO;
+import website.crm_backend.features.sales.dtos.request.UpdateLeadRequest;
+import website.crm_backend.features.sales.dtos.response.UpdateLeadResponse;
+import website.crm_backend.models.Lead;
+import website.crm_backend.models.LeadLog;
+import website.crm_backend.models.User;
+import website.crm_backend.models.enums.LeadStatus;
+import website.crm_backend.models.enums.LogAction;
+import website.crm_backend.repositories.LeadLogRepository;
+import website.crm_backend.repositories.LeadRepository;
+import website.crm_backend.repositories.UserRepository;
+import website.crm_backend.shared.mapper.LeadMapper;
+import website.crm_backend.utils.AuthUtils;
+
+@Service
+@RequiredArgsConstructor
+public class SaleService {
+    private final LeadRepository leadRepo;
+    private final UserRepository userRepo;
+    private final LeadMapper leadMapper;
+    private final LeadLogRepository leadLogRepo;
+
+    @Transactional()
+    public Page<LeadListDTO> saleGetAllLeads(Pageable pageable) {
+        int userId = AuthUtils.getUserId();
+        return leadRepo.findByAssignee_Id(userId, pageable).map(leadMapper::toListDTO);
+    }
+
+    @Transactional() 
+    public UpdateLeadResponse updateLead(int leadId, UpdateLeadRequest request) {
+        String note = request.note();
+        int actorId = AuthUtils.getUserId(); 
+        User actor = userRepo.findById(actorId)
+        .orElseThrow(() -> new IllegalArgumentException("userRepo: user not found"));
+        if(note == null || note.trim().isEmpty()) {
+            throw new IllegalArgumentException("Note cannot be null or empty"); 
+        }
+
+        Lead lead = leadRepo.findById(leadId)
+        .orElseThrow(() -> new IllegalArgumentException("leadRepo: Lead not found"));
+        
+        LeadStatus newStatus = request.status();
+        LeadStatus oldStatus = lead.getStatus();
+        lead.setNote(note);
+        
+        if(newStatus != null && newStatus != oldStatus) {
+            lead.setStatus(newStatus);
+        }
+
+        LeadLog leadLog = LeadLog.builder()
+        .lead(lead)
+        .actor(actor)
+        .action(LogAction.STATUS_CHANGE)
+        .fromStatus(oldStatus)
+        .toStatus(newStatus)
+        .build();
+
+        leadLogRepo.save(leadLog);
+        
+        return new UpdateLeadResponse(
+            lead.getNote(),
+            lead.getStatus()
+        );
+    }
+}
