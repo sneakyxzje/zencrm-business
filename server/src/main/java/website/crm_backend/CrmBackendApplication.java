75337e0bf056fa63ab444817e434f8adfc4ebf57
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 21:17:04 +0700

    feat(config): use type-safe JWT configuration properties

--- server/src/main/java/website/crm_backend/CrmBackendApplication.java
+++ server/src/main/java/website/crm_backend/CrmBackendApplication.java
@@ -2,7 +2,11 @@ package website.crm_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import website.crm_backend.config.JwtConfigProperties;
 @SpringBootApplication
+@EnableConfigurationProperties(JwtConfigProperties.class)
 public class CrmBackendApplication {
 
 	public static void main(String[] args) {
