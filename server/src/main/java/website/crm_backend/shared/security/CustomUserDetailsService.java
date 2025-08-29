ea536b811153ff73a848a072ce00ad6ee13a9543
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 16:43:45 +0700

    chore(structure): moves file

---
+++ server/src/main/java/website/crm_backend/shared/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package website.crm_backend.shared.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import website.crm_backend.domain.models.users.User;
+import website.crm_backend.domain.repositories.users.UserRepository;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException  {
+        User user = userRepository.findByEmail(email);
+        if(user == null) {
+            throw new UsernameNotFoundException("Cannot find account with email!");
+        }
+        return UserDetailsImpl.from(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(int id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found with ID: " + id));
+        return UserDetailsImpl.from(user);
+    }
+
+}
---
+++ server/src/main/java/website/crm_backend/shared/security/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package website.crm_backend.shared.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import org.springframework.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import website.crm_backend.shared.utils.CookieUtils;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private CustomUserDetailsService customUserService;
+
+    @Autowired
+    private CookieUtils cookie;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getServletPath();
+        if(path.equals("/api/auth/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = cookie.getJwtFromCookies(request);
+
+        if(token != null && jwtTokenProvider.validateToken(token)) {
+            int userId = jwtTokenProvider.getUserIdFromToken(token);
+            UserDetailsImpl userDetails = (UserDetailsImpl) customUserService.loadUserById(userId);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            logger.debug("Path " + path);
+            logger.debug("Token from cookie: " + token);
+            logger.debug("Authentication: " + SecurityContextHolder.getContext().getAuthentication());
+        }
+        else {
+            logger.debug(("Invallid token or token expired"));
+        }
+        filterChain.doFilter(request, response);
+    }
+}
