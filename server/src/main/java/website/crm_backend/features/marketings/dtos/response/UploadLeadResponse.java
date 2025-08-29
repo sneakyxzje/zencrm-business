b79863f853318cf68f371f7e20a09dd4115bec5d
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 15:43:06 +0700

    chore(structure): move files

---
+++ server/src/main/java/website/crm_backend/features/marketings/dtos/response/UploadLeadResponse.java
@@ -0,0 +1,30 @@
+package website.crm_backend.features.marketings.dtos.response;
+
+import java.time.LocalDateTime;
+
+import website.crm_backend.models.enums.LeadStatus;
+
+public record UploadLeadResponse( 
+
+    Integer id,
+    
+    Integer createdById,
+    String createdByName,
+
+    String customerName,
+    String phoneNumber,
+
+    String productName,
+
+    Integer assigneeId,
+    String assignee,
+
+    LeadStatus status,
+
+    LocalDateTime createdAt,
+    LocalDateTime assignedAt,
+
+    Integer assignedById,
+    String assignedByName 
+    ) {
+};
