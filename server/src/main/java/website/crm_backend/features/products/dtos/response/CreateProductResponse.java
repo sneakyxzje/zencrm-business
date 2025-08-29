4002195b7d16698284c24caade65f3a56d6ae6d0
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 20:30:14 +0700

    feat(product): add create product response dtos

---
+++ server/src/main/java/website/crm_backend/features/products/dtos/response/CreateProductResponse.java
@@ -0,0 +1,17 @@
+package website.crm_backend.features.products.dtos.response;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import website.crm_backend.features.category.dtos.shared.CategoryDTO;
+
+
+public record CreateProductResponse(
+    Integer id,
+    String productName,
+    BigDecimal price,
+    Integer amount,
+    Set<CategoryDTO> categories
+) {
+    
+}
