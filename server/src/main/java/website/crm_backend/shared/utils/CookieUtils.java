4241e7b0850fc910d541cbad7a9622574e5e030e
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 17:00:32 +0700

    refactor(utils): convert CookieUtils to a static utility class

--- server/src/main/java/website/crm_backend/shared/utils/CookieUtils.java
+++ server/src/main/java/website/crm_backend/shared/utils/CookieUtils.java
@@ -1,25 +1,17 @@
 package website.crm_backend.shared.utils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
-@Component
-public class CookieUtils {
-    private final Logger logger = LoggerFactory.getLogger(CookieUtils.class);
-    public String getJwtFromCookies(HttpServletRequest request) {
+public final class CookieUtils {
+    private CookieUtils() {}
+    public static String getJwtFromCookies(HttpServletRequest request) {
         if(request.getCookies() != null) {
             for(Cookie cookies : request.getCookies()) {
                 if("jwt".equals(cookies.getName())) {
                     return cookies.getValue();
                 }
             }
-        }
-        else {
-            logger.debug("No cookies found");
-            return null;
         }
         return null;
     }
