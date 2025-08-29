48699370873a7e709da68b169a6b6dac33859b20
zenitsugocrazy <tuandattobi@gmail.com>
2025-08-29 20:31:01 +0700

    feat(category): add category mapper

---
+++ server/src/main/java/website/crm_backend/shared/mapper/CategoryMapper.java
@@ -0,0 +1,27 @@
+package website.crm_backend.shared.mapper;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import website.crm_backend.domain.models.categories.Category;
+import website.crm_backend.features.category.dtos.response.CreateCategoryResponse;
+import website.crm_backend.features.products.dtos.shared.ProductDTO;
+
+@Component
+public class CategoryMapper {
+    
+    public CreateCategoryResponse toCreateCategoryResponse(Category category) {
+        Set<ProductDTO> productDTO = category.getProducts() == null ? Set.<ProductDTO>of()
+        : category.getProducts().stream()
+        .map(p -> new ProductDTO(p.getId(), p.getProductName()))
+        .collect(Collectors.toSet());
+
+        return new CreateCategoryResponse(
+            category.getId(),
+            category.getCategoryName(),
+            productDTO
+        );
+    }
+}
