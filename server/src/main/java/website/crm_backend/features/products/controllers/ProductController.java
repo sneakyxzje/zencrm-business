9c74039b47abc8732d4dfa8533ee2d8272b0cd20
zenitsugocrazy <tuandattobi@gmail.com>
2025-09-10 14:20:48 +0700

    feat: add get all product method

--- server/src/main/java/website/crm_backend/features/products/controllers/ProductController.java
+++ server/src/main/java/website/crm_backend/features/products/controllers/ProductController.java
@@ -1,15 +1,21 @@
 package website.crm_backend.features.products.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import website.crm_backend.features.products.dtos.request.CreateProductRequest;
 import website.crm_backend.features.products.dtos.response.CreateProductResponse;
+import website.crm_backend.features.products.dtos.response.GetAllProductResponse;
 import website.crm_backend.features.products.services.ProductService;
+
 
 @RestController
 @RequestMapping("/api/products")
@@ -22,5 +28,10 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<CreateProductResponse> createProduct(@RequestBody CreateProductRequest request) {
         return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @GetMapping 
+    public ResponseEntity<Page<GetAllProductResponse>> getAllProduct(@RequestParam(name="q", required = false) String q, Pageable pageable) {
+        return ResponseEntity.ok(productService.getAllProduct(q, pageable));
     }
 }
