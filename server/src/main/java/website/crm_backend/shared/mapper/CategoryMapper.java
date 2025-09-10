c084a1a3b8dbbe46638865d85badd9d15b53432e
zenitsugocrazy <tuandattobi@gmail.com>
2025-09-10 14:23:44 +0700

    feat(categories): include product price in category response

--- server/src/main/java/website/crm_backend/shared/mapper/CategoryMapper.java
+++ server/src/main/java/website/crm_backend/shared/mapper/CategoryMapper.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 import website.crm_backend.domain.models.categories.Category;
-import website.crm_backend.features.category.dtos.response.CreateCategoryResponse;
+import website.crm_backend.features.categories.dtos.response.CreateCategoryResponse;
 import website.crm_backend.features.products.dtos.shared.ProductDTO;
 
 @Component
@@ -15,7 +15,7 @@ public class CategoryMapper {
     public CreateCategoryResponse toCreateCategoryResponse(Category category) {
         Set<ProductDTO> productDTO = category.getProducts() == null ? Set.<ProductDTO>of()
         : category.getProducts().stream()
-        .map(p -> new ProductDTO(p.getId(), p.getProductName()))
+        .map(p -> new ProductDTO(p.getId(), p.getProductName(), p.getPrice()))
         .collect(Collectors.toSet());
 
         return new CreateCategoryResponse(
