c998007759e3641ad0ba917ac555cb299f89de1b
zenitsugocrazy <tuandattobi@gmail.com>
2025-09-10 14:25:22 +0700

    feat(products): expand mapper with new DTO mappings

--- server/src/main/java/website/crm_backend/shared/mapper/ProductMapper.java
+++ server/src/main/java/website/crm_backend/shared/mapper/ProductMapper.java
@@ -5,12 +5,23 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
+import website.crm_backend.domain.models.categories.Category;
 import website.crm_backend.domain.models.products.Product;
-import website.crm_backend.features.category.dtos.shared.CategoryDTO;
+import website.crm_backend.features.categories.dtos.shared.CategoryDTO;
 import website.crm_backend.features.products.dtos.response.CreateProductResponse;
+import website.crm_backend.features.products.dtos.response.GetAllProductResponse;
+import website.crm_backend.features.products.dtos.shared.ProductDTO;
 
 @Component
 public class ProductMapper {
+
+    public ProductDTO toProductDTO(Product p) {
+        return new ProductDTO(
+            p.getId(),
+            p.getProductName(),
+            p.getPrice()
+        );
+    }
     public CreateProductResponse toCreateProductResponse(Product p) {
         Set<CategoryDTO> categoryDTO = p.getCategories() == null ? Set.<CategoryDTO>of()
         : p.getCategories().stream()
@@ -22,6 +33,17 @@ public class ProductMapper {
             p.getPrice(),
             p.getAmount(),
             categoryDTO
+        );
+    }
+    public GetAllProductResponse toGetAllProductResponse(Product p) {
+        Set<String> categoryNames = p.getCategories().stream()
+        .map(Category::getCategoryName)
+        .collect(Collectors.toSet());
+
+        return new GetAllProductResponse(
+            p.getId(),
+            p.getProductName(),
+            categoryNames
         );
     }
 }
