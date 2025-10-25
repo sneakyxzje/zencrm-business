55f3e906f02e657d598c937cde1694060c70dba5
zenitsugocrazy <tuandattobi@gmail.com>
2025-10-25 16:35:43 +0700

    feat(product): expand GetAllProductResponse DTO with price, amount, and image

--- server/src/main/java/website/crm_backend/features/products/dtos/response/GetAllProductResponse.java
+++ server/src/main/java/website/crm_backend/features/products/dtos/response/GetAllProductResponse.java
@@ -1,10 +1,15 @@
 package website.crm_backend.features.products.dtos.response;
 
+import java.math.BigDecimal;
 import java.util.Set;
+
 
 public record GetAllProductResponse(
     int id,
     String productName,
+    int amount,
+    BigDecimal price,
+    String imageUrl,
     Set<String> categoryName
 ) {
     
