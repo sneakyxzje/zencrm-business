f48cac5629d069a904757279a2ddb6ee347f7f96
zenitsugocrazy <tuandattobi@gmail.com>
2025-10-25 16:34:45 +0700

    feat(product): implement combo offer processing and expand details in createProduct

--- server/src/main/java/website/crm_backend/features/products/services/ProductService.java
+++ server/src/main/java/website/crm_backend/features/products/services/ProductService.java
@@ -12,10 +12,14 @@ import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import website.crm_backend.domain.models.categories.Category;
+import website.crm_backend.domain.models.products.Combo;
+import website.crm_backend.domain.models.products.Gift;
 import website.crm_backend.domain.models.products.Product;
 import website.crm_backend.domain.repositories.categories.CategoryRepository;
+import website.crm_backend.domain.repositories.products.GiftRepository;
 import website.crm_backend.domain.repositories.products.ProductRepository;
 import website.crm_backend.domain.repositories.products.specs.ProductSpec;
+import website.crm_backend.features.combos.shared.CreateComboRequest;
 import website.crm_backend.features.products.dtos.request.CreateProductRequest;
 import website.crm_backend.features.products.dtos.response.CreateProductResponse;
 import website.crm_backend.features.products.dtos.response.GetAllProductResponse;
@@ -27,25 +31,48 @@ public class ProductService {
     private final ProductRepository productRepo;
     private final CategoryRepository categoryRepo;
     private final ProductMapper productMapper;
-
+    private final GiftRepository giftRepo;
     @Transactional
     public CreateProductResponse createProduct(CreateProductRequest request) {
         Set<Category> categories = new HashSet<>();
         if (request.categoryId() != null && !request.categoryId().isEmpty()) {
-        categories = new HashSet<>(
+            categories = new HashSet<>(
             categoryRepo.findAllById(request.categoryId())
-        );
-         }
+            );
+        }
+
         Product product = Product.builder()
         .productName(request.productName())
         .price(request.price())
         .amount(request.amount())
         .categories(categories)
+        .baseUnitName(request.baseUnitName())
+        .packageUnitName(request.packageUnitName())
+        .itemsPerPackage(request.itemsPerPackage())
+        .imageUrl(request.imageUrl())
         .build();
+        if(request.combos() != null && !request.combos().isEmpty()) {
+            for (CreateComboRequest comboRequest : request.combos()) {
+                Gift gift = null;
+                if(comboRequest.giftItemId() != null) {
+                    gift = giftRepo.getReferenceById(comboRequest.giftItemId());
+                }
+                Combo newCombo = Combo.builder()
+                .offerName(comboRequest.offerName())
+                .requiredQuantity(comboRequest.requiredQuantity())
+                .giftItem(gift)
+                .giftQuantity(comboRequest.giftQuantity())
+                .isMandatory(comboRequest.isMandatory())
+                .isActive(true)
+                .mainProduct(product)
+                .build();
+                product.getComboOffer().add(newCombo);
+            }
+        } 
         productRepo.save(product);
-        
         return productMapper.toCreateProductResponse(product);
     }
+
     public Page<GetAllProductResponse> getAllProduct(String q, Pageable pageable) {
         Specification<Product> spec = (r, query, cb) -> cb.conjunction();
         if (q != null && !q.trim().isEmpty()) {
