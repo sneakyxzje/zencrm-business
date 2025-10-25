74c74e6cef1194e01aa5afcb574f7cce929efe70
zenitsugocrazy <tuandattobi@gmail.com>
2025-10-25 16:49:55 +0700

    feat(user): add ROLE_WAREHOUSE to UserRole enum

--- server/src/main/java/website/crm_backend/domain/models/users/enums/UserRole.java
+++ server/src/main/java/website/crm_backend/domain/models/users/enums/UserRole.java
@@ -2,5 +2,5 @@ package website.crm_backend.domain.models.users.enums;
 
 public enum UserRole {
     ROLE_MARKETING, ROLE_SALE, ROLE_ADMIN, ROLE_SALE_MANAGER,
-    ROLE_MARKETING_MANAGER
+    ROLE_MARKETING_MANAGER, ROLE_WAREHOUSE
 }
