172833e7fb354046866968f986fdbbe9e226525d
zenitsugocrazy <tuandattobi@gmail.com>
2025-11-17 16:16:15 +0700

    refactor(order): remove redundant fields from CreateOrderRequest

--- server/src/main/java/website/crm_backend/features/orders/dtos/request/CreateOrderRequest.java
+++ server/src/main/java/website/crm_backend/features/orders/dtos/request/CreateOrderRequest.java
@@ -2,13 +2,10 @@ package website.crm_backend.features.orders.dtos.request;
 
 import java.math.BigDecimal;
 public record CreateOrderRequest(
-    String customerName,
+    Integer leadId,
     String address,
     String phoneNumber,
-    Integer productId,
-    Integer leadId,
-    Integer marketingId,
-    Integer saleId,
+    String customerName,
     BigDecimal priceAtOrder
 ) {
     
