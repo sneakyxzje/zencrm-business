ae9fb5054e6bae6e2afe76d0c7ce73a01ba2c1c7
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-05 14:33:23 +0700

    feat: add search params

--- server/src/main/java/website/crm_backend/features/marketings/controllers/MarketingController.java
+++ server/src/main/java/website/crm_backend/features/marketings/controllers/MarketingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -29,7 +30,7 @@ public class MarketingController {
     } 
 
     @GetMapping
-    public ResponseEntity<Page<LeadListDTO>> getAllLeads(Pageable pageable) {
-        return ResponseEntity.ok(marketingService.getAllLeads(pageable));
+    public ResponseEntity<Page<LeadListDTO>> getAllLeads(@RequestParam(required = false) String search, Pageable pageable) {
+        return ResponseEntity.ok(marketingService.getAllLeads(search, pageable));
     }
 }
