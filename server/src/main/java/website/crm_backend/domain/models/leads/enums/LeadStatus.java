0e751e5687ad5bfa85a744cc5aa622563a06591a
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-11 18:19:55 +0700

    chore: remove comments

--- server/src/main/java/website/crm_backend/domain/models/leads/enums/LeadStatus.java
+++ server/src/main/java/website/crm_backend/domain/models/leads/enums/LeadStatus.java
@@ -1,15 +1,6 @@
 package website.crm_backend.domain.models.leads.enums;
 
 
-/*
-
-    NEW - Chưa gán
-    ASSIGNED - Đã gán
-    PROCESSING - Đang chăm sóc
-    READY_TO_ORDER - Chốt, sẵn sàng mua
-    WIN - Đơn hàng giao thành công
-    CLOSED - Số đóng huỷ
-*/
 public enum LeadStatus {
     NEW,
     ASSIGNED,
