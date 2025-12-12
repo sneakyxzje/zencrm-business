f56d1cd24cda5e7bd479b771dbb130d7ab29ed89
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-12 14:16:17 +0700

    fix(lead): make status nullable in UpdateLeadPayLoad

--- client/src/features/update-lead/model/types.ts
+++ client/src/features/update-lead/model/types.ts
@@ -3,7 +3,7 @@ import type { LeadStatus } from "@entities/lead/model/types";
 export type UpdateLeadPayLoad = {
   leadId: number;
   note: string;
-  status: string;
+  status: string | null;
 };
 
 export type UpdateLeadResponse = {
