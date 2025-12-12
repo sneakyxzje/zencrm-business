4a430ee5e516d296cc5f4b870b088c20b9fa8b65
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-12 13:59:57 +0700

    feat(product): add interfaces for product creation, combos, and form state

--- client/src/entities/product/model/type.ts
+++ client/src/entities/product/model/type.ts
@@ -3,3 +3,30 @@ export type GetAllProduct = {
   productName: string;
   categoryName: string;
 };
+
+export interface FormProductState {
+  productName: string;
+  amount: number;
+  price: string;
+  categoryId: Set<number>;
+  baseUnitName: string;
+  packageUnitName: string;
+  itemsPerPackage: number;
+  imageUrl: string;
+}
+
+export interface ComboFormState {
+  offerName: string;
+  requiredQuantity: number;
+  giftItemId: number | "";
+  giftQuantity: number;
+  isMandatory: boolean;
+  startDate: string;
+  endDate: string;
+}
+
+export interface CreateProductRequest
+  extends Omit<FormProductState, "categoryId"> {
+  categoryId: number[];
+  combos: ComboFormState[];
+}
