ef46a729b47d87ed8a67553ce47783eeedcbfc40
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-12 14:32:41 +0700

    refactor(marketing): move LeadUploadForm to shared features and remove redundant sidebar component

--- client/src/pages/Marketing/upload/MarketingUpload.tsx
+++ client/src/pages/Marketing/upload/MarketingUpload.tsx
@@ -1,12 +1,10 @@
 import { useAuth } from "@processes/auth/models/useAuth";
-import LeadUploadForm from "@features/lead-upload/ui/LeadUploadForm";
-import Sidebar from "@widgets/sidebar/ui/sidebar";
-import { menuByRole } from "@widgets/sidebar/model/items";
 import { useEffect, useState } from "react";
 import { getAssignableSales } from "@entities/user/api";
 import type { AssignableSales } from "@entities/user/model/types";
 import { getAllProducts } from "@entities/product/api";
 import type { GetAllProduct } from "@entities/product/model/type";
+import LeadUploadForm from "@features/shared/upload-lead/ui/LeadUploadForm";
 
 export default function MarketingUploadPage() {
   const { user } = useAuth();
@@ -34,7 +32,6 @@ export default function MarketingUploadPage() {
   const defaultAssigneeId = sale && sale.length > 0 ? sale[0].id : null;
   return (
     <div className="min-h-screen bg-[#232629] flex">
-      <Sidebar sideBarItems={menuByRole.ROLE_MARKETING} />
       <main className="flex-1 p-6">
         <LeadUploadForm
           currentUserName={user?.username ?? ""}
