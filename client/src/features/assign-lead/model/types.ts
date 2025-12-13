73775f7f4bff0394f132b1182e529dc092b2b18c
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-13 15:41:14 +0700

    chore: remove unused files

--- client/src/features/assign-lead/model/types.ts
+++
@@ -1,8 +0,0 @@
-export type AssignLeadPayload = { leadId: number; saleId: number };
-export type AssignLeadResponse = {
-  saleId: number;
-  saleName: string;
-  status: string;
-  assignedAt: string;
-  log?: { id: number; action: "ASSIGN"; actorName: string; createdAt: string };
-};
