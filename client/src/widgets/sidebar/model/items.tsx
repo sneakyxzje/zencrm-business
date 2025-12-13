62ecb41936992be35973ab1699d34264875b2903
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-13 15:48:26 +0700

    refactor: remove unnecessary icon

--- client/src/widgets/sidebar/model/items.tsx
+++ client/src/widgets/sidebar/model/items.tsx
@@ -2,15 +2,11 @@ import type { SidebarItems, Role } from "./types";
 
 const marketingMenu: SidebarItems[] = [
   { id: 1, name: "Home", icon: "home", path: "/" },
-  { id: 2, name: "Announcement", icon: "announcement", path: "/announcement" },
   { id: 3, name: "Customers", icon: "customers", path: "/customers" },
-  { id: 4, name: "Campaigns", icon: "campaigns", path: "/campaigns" },
-  { id: 5, name: "Order Materials", icon: "materials", path: "/orders" },
 ];
 
 const saleMenu: SidebarItems[] = [
   { id: 1, name: "Home", icon: "home", path: "/" },
-  { id: 2, name: "Announcement", icon: "announcement", path: "/announcement" },
   { id: 3, name: "Customers", icon: "customers", path: "/customers" },
 ];
 
